e8811ddd32f711a72a41585138a4c8b7ae0cce31
Devansh Singh <nsddownloaderbot@gmail.com>
2024-10-11 23:40:36 +0530

    Add files via upload

---
+++ Day 1/index.py
@@ -0,0 +1,14 @@
+# import os
+
+# path=os.chdir(r"C:\Users\91700\OneDrive\Pictures\back\Sigma web development course")
+# i=0
+# for file in os.listdir(path):
+#     new_name=(f"iger and Buffalo pic ({i}).pngT")
+#     os.rename(file,new_name)
+#     i=i+1
+
+import os 
+
+for i in range(1,100):
+    os.mkdir(f"Day {i}")
+
